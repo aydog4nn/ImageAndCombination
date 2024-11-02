49e1be61d74efb7a71f9d4407ba671de58eea410
aydog4nn <zephyrflaner@gmail.com>
2024-11-02 17:32:19 +0300

    updated-get-post-request

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,77 +1,184 @@
-import React from 'react'
-import {useState} from "react";
-import {useSelector} from "react-redux";
-import {useEffect} from "react";
-import {useDispatch} from "react-redux";
-import {addCategory, fetchCategoryById} from "../redux/slices/CategoriesSlice.jsx";
-import {Link} from "react-router-dom";
-import Form from 'react-bootstrap/Form';
-import {Button} from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addCategory, fetchCategoryById, fetchAllCategory , addProducts} from "../redux/slices/CategoriesSlice.jsx";
+import Form from "react-bootstrap/Form";
+import { Button, Container } from "react-bootstrap";
 
 function HomePage() {
-
-
     const dispatch = useDispatch();
+
+    // Kategori bilgileri için state
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
-    const category = useSelector((state) => state.categories.category);
 
-    const handleSubmit = (e) => {
+    // Ürün bilgileri için state
+    const [productData, setProductData] = useState({
+        size: "",
+        name: "",
+        material: "",
+        brand: "",
+        price: 0,
+        categoryId: 1
+    });
+
+    const category = useSelector((state) => state.categories.category);
+    const allCategory = useSelector((state) => state.categories.allCategories);
+
+    // Burası kategori işlemleri için yapılan fonksiyon
+    const handleCategorySubmit = (e) => {
         e.preventDefault();
         if (name && description) {
-            console.log("Gönderilen Kategori:", {name, description});
-            dispatch(addCategory({name, description}));
+            console.log("Gönderilen Kategori:", { name, description });
+            dispatch(addCategory({ name, description }));
             setName("");
             setDescription("");
         } else {
             alert("İsim ve açıklama gerekli!");
         }
-    }
+    };
+
+    const handleProductSubmit = (e) => {
+        e.preventDefault();
+        console.log("Gönderilen Ürün:", productData);
+        // Ürün verisini göndermek için dispatch ile ilgili bir action eklenebilir
+        setProductData({ size: "", name: "", material: "", brand: "", price: 0 });
+        dispatch(addProducts(productData))
+    };
+
+    const handleProductChange = (e) => {
+        const { name, value } = e.target;
+        setProductData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
 
     useEffect(() => {
         const fetchCategory = async () => {
-            if (category.id) { // category.id'nin tanımlı olup olmadığını kontrol et
+            if (category.id) {
                 await dispatch(fetchCategoryById(category.id));
             }
         };
 
-        fetchCategory(); // Asenkron fonksiyonu çağır
-    }, [dispatch, category.id]); // Bağımlılıklar
-
+        fetchCategory();
+    }, [dispatch, category.id]);
 
     return (
-        <div className="container">
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Container className="container">
+            <h2>Kategori Ekle</h2>
+            <Form onSubmit={handleCategorySubmit}>
+                <Form.Group className="mb-3" controlId="categoryName">
                     <Form.Label>Kategori</Form.Label>
-                    <Form.Control value={name} onChange={(e) => setName(e.target.value)} type="text"
-                                  placeholder="Kategori giriniz..." required/>
+                    <Form.Control
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        type="text"
+                        placeholder="Kategori giriniz..."
+                        required
+                    />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Group className="mb-3" controlId="categoryDescription">
                     <Form.Label>Açıklamayı giriniz</Form.Label>
-                    <Form.Control value={description} onChange={(e) => setDescription(e.target.value)} as="textarea"
-                                  rows={3} required/>
+                    <Form.Control
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        as="textarea"
+                        rows={3}
+                        required
+                    />
                 </Form.Group>
-                <Button variant="primary" type="submit">ekle</Button>
+                <Button variant="primary" type="submit">
+                    Ekle
+                </Button>
+            </Form>
+
+            <h2>Ürün Ekle</h2>
+            <Form onSubmit={handleProductSubmit}>
+                <Form.Group className="mb-3" controlId="productSize">
+                    <Form.Label>Size</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Size giriniz..."
+                        name="size"
+                        value={productData.size}
+                        onChange={handleProductChange}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="productName">
+                    <Form.Label>Ürün Adı</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Ürün adı giriniz..."
+                        name="name"
+                        value={productData.name}
+                        onChange={handleProductChange}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="productMaterial">
+                    <Form.Label>Malzeme</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Malzeme giriniz..."
+                        name="material"
+                        value={productData.material}
+                        onChange={handleProductChange}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="productBrand">
+                    <Form.Label>Marka</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Marka giriniz..."
+                        name="brand"
+                        value={productData.brand}
+                        onChange={handleProductChange}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="productPrice">
+                    <Form.Label>Fiyat</Form.Label>
+                    <Form.Control
+                        type="number"
+                        placeholder="Fiyat giriniz..."
+                        name="price"
+                        value={productData.price}
+                        onChange={handleProductChange}
+                    />
+                </Form.Group>
+                <Form.Group controlId="productCategoryId">
+                    <Form.Label>Kategori Seçin</Form.Label>
+                    <Form.Control
+                        as="select"
+                        name="categoryId"
+                        value={productData.categoryId}
+                        onChange={handleProductChange}
+                    >
+                        {allCategory.map((category) => (
+                            <option key={category.id} value={category.id}>
+                                {category.name}
+                            </option>
+                        ))}
+                    </Form.Control>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Ürün Ekle
+                </Button>
             </Form>
 
             <div>
                 <h2>Kategoriler</h2>
-                <ul>
-                    {category && category.length > 0 ?
-                        category.map((cat) => (
-                            <li key={cat.id}>
-                                <Link to={`/category/${cat.id}`}>{cat.name}</Link>
-                            </li>
-                        )) : (
-                            <li> Hiç Kategori değeri bulunamadı!</li>
-                        )
-                    }
-                </ul>
+                <div>{category.name}</div>
+                <div>{category.description}</div>
             </div>
 
-        </div>
+
+            <button onClick={() => dispatch(fetchCategoryById(1))}>Kategori Getir</button>
+            <button onClick={() => dispatch(fetchAllCategory())}>Bütün Kategorileri Getir</button>
+        </Container>
     );
 }
 
-export default HomePage
+export default HomePage;
--- src/redux/slices/CategoriesSlice.jsx
+++ src/redux/slices/CategoriesSlice.jsx
@@ -1,37 +1,52 @@
 import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
-import axios from "axios"
-import * as action from "react-dom/test-utils";
+import axios from "axios";
 
 const initialState = {
+    allCategories: [],
     category: [],
+    products: [],
     loading: false,
     error: null,
 };
 
 const BASE_URL = "http://localhost:8080/api/category";
+const BASE_URL_PRODUCTS = "http://localhost:8080/api/products";
 
 export const fetchCategoryById = createAsyncThunk(
-    "categories/fetchCategoriesById",
+    "categories/fetchCategoryById",
     async (id) => {
         const response = await axios.get(`${BASE_URL}/${id}`);
         console.log(response.data);
         return response.data;
     }
+);
 
+export const fetchAllCategory = createAsyncThunk(
+    "categories/fetchAllCategory",
+    async () => {
+        const response = await axios.get(BASE_URL);
+        console.log(response.data)
+        return response.data;
+    }
+);
 
-    )
-
+export const addProducts = createAsyncThunk(
+    "categories/addProducts",
+    async (products) => {
+        const response = await axios.post(BASE_URL_PRODUCTS, products);
+        console.log("Gönderilen ürünler:", response.data);
+        return response.data;
+    }
+)
 
 export const addCategory = createAsyncThunk(
     "categories/addCategory",
     async (category) => {
         const response = await axios.post(BASE_URL, category);
-        console.log("Gönderilen veri:",response.data);
+        console.log("Gönderilen veri:", response.data);
         return response.data;
     }
-)
-
-
+);
 
 const CategoriesSlice = createSlice({
     name: "categories",
@@ -39,31 +54,40 @@ const CategoriesSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
-            .addCase(addCategory.pending,(state) => {
-                state.loading=true;
+            .addCase(addCategory.pending, (state) => {
+                state.loading = true;
             })
             .addCase(addCategory.fulfilled, (state, action) => {
                 state.category.push(action.payload);
-            })
-            .addCase(addCategory.rejected,(state,action) => {
                 state.loading = false;
-                state.error = action.error.message
             })
-            .addCase(fetchCategoryById.pending,(state) => {
-                state.loading=true;
-            })
-            .addCase(fetchCategoryById.fulfilled, (state, action) => {
-                state.loading=false;
-                state.category = action.payload;
-            })
-            .addCase(fetchCategoryById.rejected, (state, ) => {
-                state.loading=false;
+            .addCase(addCategory.rejected, (state, action) => {
+                state.loading = false;
                 state.error = action.error.message;
             })
+            .addCase(fetchCategoryById.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(fetchCategoryById.fulfilled, (state, action) => {
+                state.loading = false;
+                state.category = action.payload;
+            })
+            .addCase(fetchCategoryById.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            })
+            .addCase(fetchAllCategory.fulfilled, (state, action) => {
+                state.loading = false;
+                state.allCategories = action.payload;
+            })
+            .addCase(addProducts.fulfilled, (state,action) => {
+                state.loading = false;
+                state.products.push(action.payload)
+            })
     }
-})
+});
 
-export const selectCategoryById = (state,id) =>
-    state.category.categories.find(category => category.id === id)
+export const selectCategoryById = (state, id) =>
+    state.category.categories.find((category) => category.id === id);
 
 export default CategoriesSlice.reducer;
