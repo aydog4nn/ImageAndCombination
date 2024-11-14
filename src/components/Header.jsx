492c195003163959bcaaa58ec97e91c6b6cb470f
aydog4nn <zephyrflaner@gmail.com>
2024-11-14 21:49:55 +0300

    header-css-edited-and-updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,13 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import styled from 'styled-components';
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { BsTruck } from "react-icons/bs";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {BsTruck} from "react-icons/bs";
+import {BsBag} from "react-icons/bs";
+import {RiAccountBoxLine} from "react-icons/ri";
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 
 function Header() {
     const navigate = useNavigate();
@@ -28,7 +32,7 @@ function Header() {
             content: "";
             position: absolute;
             left: 50%;
-            bottom: -7px; 
+            bottom: -7px;
             width: 0;
             height: 2px;
             background-color: #000;
@@ -47,28 +51,28 @@ function Header() {
     };
 
     const DrawerList = (
-        <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+        <Box sx={{width: 250}} role="presentation" onClick={toggleDrawer(false)}>
             <List>
                 {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
                     <ListItem key={text} disablePadding>
                         <ListItemButton>
                             <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
                             </ListItemIcon>
-                            <ListItemText primary={text} />
+                            <ListItemText primary={text}/>
                         </ListItemButton>
                     </ListItem>
                 ))}
             </List>
-            <Divider />
+            <Divider/>
             <List>
                 {['All mail', 'Trash', 'Spam'].map((text, index) => (
                     <ListItem key={text} disablePadding>
                         <ListItemButton>
                             <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
                             </ListItemIcon>
-                            <ListItemText primary={text} />
+                            <ListItemText primary={text}/>
                         </ListItemButton>
                     </ListItem>
                 ))}
@@ -90,7 +94,7 @@ function Header() {
                     fontSize: "30px",
                     color: "#f7e7d4",
                     marginRight: "10px",
-                }} />
+                }}/>
                 <h6 style={{
                     color: "#f7e7d4",
                     margin: 0,
@@ -100,27 +104,42 @@ function Header() {
             </div>
             <div className="bg-light d-flex justify-content-around align-items-center">
 
-            <div className="bg-light d-flex justify-content-start align-items-center"
-                 style={{
-                     height: "70px",
-                     gap: "20px",
-                     fontFamily: "'Geist Mono', monospace",
-                 }}>
-                <Button style={{
-                    color: "#000",
-                    fontSize: "15px",
-                    border: "1px solid black",
-                    borderRadius: "5px",
-                    padding: "5px 25px"
-                }} onClick={toggleDrawer(true)}>MENU</Button>
-                <Drawer open={open} onClose={toggleDrawer(false)}>
-                    {DrawerList}
-                </Drawer>
-                <UnderlinedText><h6 style={{ margin: 0 }}>Erkek</h6></UnderlinedText>
-                <UnderlinedText><h6 style={{margin: 0,}}>Kadın</h6></UnderlinedText>
-            </div>
-                <div><h2>GDG INONU</h2></div>
-                <div></div>
+                <div className="bg-light d-flex justify-content-start align-items-center"
+                     style={{
+                         height: "70px",
+                         gap: "20px",
+                         fontFamily: "'Geist Mono', monospace",
+                     }}>
+                    <Button style={{
+                        color: "#000",
+                        fontSize: "15px",
+                        border: "1px solid black",
+                        borderRadius: "5px",
+                        padding: "5px 25px"
+                    }} onClick={toggleDrawer(true)}>MENU</Button>
+                    <Drawer open={open} onClose={toggleDrawer(false)}>
+                        {DrawerList}
+                    </Drawer>
+                    <UnderlinedText><h6 style={{margin: 0}}>Erkek</h6></UnderlinedText>
+                    <UnderlinedText><h6 style={{margin: 0,}}>Kadın</h6></UnderlinedText>
+                </div>
+
+                <div>
+                    <Typography sx={{
+                        fontWeight: "bold",
+                        letterSpacing: "0.15em",
+                        textTransform:"uppercase"
+                    }} variant="h5">Diggy&mia</Typography>
+                </div>
+
+                <div className="d-flex justify-content-around gap-3 align-items-center fs-5">
+                    <div><TextField id="outlined-basic" size="small" color="success" label="Outlined"
+                                    variant="outlined"/></div>
+                    <div><BsBag/></div>
+                    <div style={{
+                        fontSize: "25px"
+                    }}><RiAccountBoxLine/></div>
+                </div>
             </div>
         </div>
     );
