ba933894201d86ed93e4a50e133b990d7f84019c
aydog4nn <zephyrflaner@gmail.com>
2024-12-16 20:53:48 +0300

    added-productDetailsPage

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,34 +1,131 @@
-import { useSelector, useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
-import { useEffect } from "react";
-import { fetchProductsById, setSelectedProduct } from "../redux/slices/ProductSlice.jsx";
-
-
+import  {useEffect} from 'react';
+import {
+    Box,
+    Button,
+    Card,
+    CardMedia,
+    Typography,
+    Grid,
+    IconButton,
+    Container,
+    Divider,
+    Stack,
+    Select,
+    MenuItem
+} from '@mui/material';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import {useDispatch, useSelector} from "react-redux";
+import {fetchAllProduct, fetchProductsById} from "../redux/slices/ProductSlice.jsx";
+import {useNavigate, useParams} from "react-router-dom";
 
 function ProductDetails() {
-    const { id } = useParams(); // URL'den ürün ID'sini al
+    const navigate = useNavigate();
     const dispatch = useDispatch();
-    const selectedProduct = useSelector((state) => state.products.selectedProduct); // Seçilen ürünü Redux store'dan al
-
+    const {id} = useParams()
+    const selectedProduct = useSelector((state) => state.products.selectedProduct);
+    const allProducts = useSelector((state) => state.products.allProducts);
     useEffect(() => {
-        dispatch(fetchProductsById(id));
-    }, [dispatch, id]);
-
-    if (!selectedProduct) {
-        return <p>Yükleniyor...</p>;
-    }
-
-    const { name, size, material, brand, price } = selectedProduct;
+        dispatch(fetchAllProduct())
+        dispatch(fetchProductsById(id))
+    }, [dispatch,id]);
 
     return (
+        <Container maxWidth="lg">
 
-        <div>
-            <h2>{name}</h2>
-            <p>Boyut: {size}</p>
-            <p>Malzeme: {material}</p>
-            <p>Marka: {brand}</p>
-            <p>Fiyat: {price} TL</p>
-        </div>
+                    <Box key={selectedProduct.id} mt={4}>
+                        {/* Sol tarafta ürün resmi */}
+                        <Grid container spacing={4}>
+                            <Grid item xs={12} md={6}>
+                                <Card>
+                                    <CardMedia
+                                        component="img"
+                                        height="500"
+                                        image={`http://localhost:8080/api/products/file/${selectedProduct.imageName}`} // Resim URL'sini buraya ekleyin.
+                                        alt="Light Gray Top"
+                                    />
+                                </Card>
+                            </Grid>
+
+                            {/* Sağ tarafta Ürün Bilgisi */}
+                            <Grid item xs={12} md={6}>
+                                <Stack spacing={2}>
+                                    <Typography variant="h5" component="h1">
+                                        {selectedProduct.name}
+                                    </Typography>
+                                    <Typography variant="h4" color="success">
+                                        {selectedProduct.price} TL
+                                    </Typography>
+                                    <Typography variant="body1" color="text.secondary">
+                                        Light gray solid Top, has a boat neck, 3/4 sleeves
+                                    </Typography>
+
+                                    {/* Beden Seçimi */}
+                                    <Box>
+                                        <Typography variant="body2">Select Size:</Typography>
+                                        <Select defaultValue={selectedProduct.size} fullWidth>
+                                            <MenuItem value="S">S</MenuItem>
+                                            <MenuItem value="M">M</MenuItem>
+                                            <MenuItem value="L">L</MenuItem>
+                                            <MenuItem value="XL">XL</MenuItem>
+                                        </Select>
+                                    </Box>
+
+                                    {/* Butonlar */}
+                                    <Stack direction="row" spacing={2}>
+                                        <Button variant="contained" color="success" startIcon={<AddShoppingCartIcon/>}>Sepete ekle</Button>
+                                        <IconButton color="success">
+                                            <FavoriteBorderIcon/>
+                                        </IconButton>
+                                    </Stack>
+
+                                    <Divider/>
+                                    <Typography variant="caption" color="text.secondary">
+                                        Wind & Shine, Softwear <br/>
+                                        Material: Cotton <br/>
+                                        Machine Wash
+                                    </Typography>
+                                </Stack>
+                            </Grid>
+                        </Grid>
+                    </Box>
+
+
+            {/* Ana Sayfa Kutusu */}
+
+
+            {/* Benzer Ürünler Bölümü */}
+            <Box  mt={6}>
+                <Typography variant="h6" mb={2}>Benzer ürünler</Typography>
+                <Grid container spacing={2}>
+                    {[...allProducts]
+                        .filter(item => item.gender === selectedProduct.gender)
+                        .sort(() => Math.random() - 0.5)
+                        .slice(0,4)
+                        .map((item) => (
+                        <Grid  item xs={6} sm={3} key={item.id}>
+                            <Card onClick={() => navigate(`/products/${item.id}`)} sx={{
+                                cursor:"pointer",
+                            }}>
+                                <CardMedia
+                                    component="img"
+                                    height="300"
+                                    image={`http://localhost:8080/api/products/file/${item.imageName}`}
+                                    alt={`Product ${item}`}
+                                />
+                                <Box p={1}>
+                                    <Typography variant="body2">Product {item.name}</Typography>
+                                    <Typography variant="subtitle2" color="primary">₺{ item.price }</Typography>
+                                </Box>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Box>
+            <div style={{
+                height:"100px"
+            }}></div>
+        </Container>
     );
 }
 
--- src/pages/CreateCombinatePage.jsx
+++ src/pages/CreateCombinatePage.jsx
@@ -1,24 +1,22 @@
-import { Container, Row, Col, Card } from 'react-bootstrap';
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchProductsByPersonal, uploadImage } from "../redux/slices/AdviceSlice.jsx";
-import { useNavigate } from "react-router-dom";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import { CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { uploadImage, fetchProductsByPersonal } from '../redux/slices/AdviceSlice'; // Redux slice'ını import ediyoruz
+import { useNavigate } from 'react-router-dom';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { IconButton } from "@mui/material";
+import { useState as useLikeState } from "react";
 
 function CreateCombinatePage() {
-    const [selectedImage, setSelectedImage] = useState(null);
     const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const userID = localStorage.getItem("userID");
-    const { personalProducts, loading, error } = useSelector(state => state.advice);
-    const [likedProducts, setLikedProducts] = useState([]); // Favori durumu için state
+    const [selectedImage, setSelectedImage] = useState(null);
 
-    useEffect(() => {
-        if (userID) {
-            dispatch(fetchProductsByPersonal(userID)); // Sayfa yüklendiğinde ürünleri al
-        }
-    }, [dispatch, userID]);
+    // Redux'tan gelen veriyi alıyoruz
+    const { personalProducts, loading, error } = useSelector((state) => state.advice);
 
     const handleImageChange = (e) => {
         const file = e.target.files[0];
@@ -29,153 +27,203 @@ function CreateCombinatePage() {
         e.preventDefault();
 
         if (selectedImage) {
-            dispatch(uploadImage({ imageFile: selectedImage, id: userID }));
+            const userID = localStorage.getItem('userID'); // Kullanıcı ID'sini localStorage'dan alıyoruz
+            dispatch(uploadImage({ imageFile: selectedImage, id: userID })); // Resmi Redux'a gönderiyoruz
         } else {
-            console.log("Lütfen bir fotoğraf seçiniz!");
+            console.log("Lütfen bir resim seçin!");
         }
     };
 
-    const handleLike = (productId) => {
-        setLikedProducts((prevLiked) =>
-            prevLiked.includes(productId)
-                ? prevLiked.filter((id) => id !== productId)
-                : [...prevLiked, productId]
-        );
-    };
-
-    const Product = ({ product }) => {
-        if (!product) {
-            return <div>Ürün bulunamadı</div>;
+    useEffect(() => {
+        const userID = localStorage.getItem('userID');
+        if (userID) {
+            dispatch(fetchProductsByPersonal(userID)); // Kullanıcıya ait ürünleri çekiyoruz
         }
-
-        const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "black"];
-        const { id, name, size, material, brand, price, imageName } = product;
-
-        return (
-            <Col key={id} sm={12} md={6} lg={4}>
-                <Card className="mb-4">
-                    <CardActionArea onClick={() => navigate(`/products/${id}`)}>
-                        <CardMedia
-                            component="img"
-                            height="500"
-                            image={imageName ? `http://localhost:8080/api/products/file/${imageName}` : "http://localhost:8080/api/products/file/product_2_05435615-152f-45d0-b756-4219f563f306.jpg"}
-                            alt={name}
-                        />
-                        <CardContent style={{ textAlign: "center", fontSize: "5px" }}>
-                            <Typography style={{ textTransform: "uppercase", fontFamily: "'Platypi', 'serif'" }} gutterBottom variant="caption" component="div">
-                                {name}
-                            </Typography>
-                            <Typography style={{ textTransform: "uppercase", fontFamily: "'Platypi', 'serif'", fontWeight: "bolder" }} variant="body2">
-                                {price},00 TL
-                            </Typography>
-                            <div
-                                onClick={(e) => {
-                                    e.stopPropagation(); // Kartın tıklanmasını engellemek için
-                                    handleLike(id);
-                                }}
-                                style={{
-                                    position: "absolute",
-                                    bottom: "20px", // Alt kısmın biraz üstüne
-                                    right: "20px", // Sağ tarafta
-                                    backgroundColor: "white", // Arka plan beyaz
-                                    borderRadius: "50%",
-                                    padding: "10px",
-                                    cursor: "pointer",
-                                    display: "flex",
-                                    justifyContent: "center",
-                                    alignItems: "center",
-                                    boxShadow: likedProducts.includes(id) ? "0 4px 8px rgba(0, 0, 0, 0.3)" : "0 4px 8px rgba(0, 0, 0, 0.1)", // Gölge
-                                }}
-                            >
-                                <FavoriteIcon style={{ color: likedProducts.includes(id) ? "red" : "black" }} />
-                            </div>
-                            <div style={{
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                                gap: "4px",
-                                marginTop: "10px",
-                            }}>
-                                {colors.map((color, index) => (
-                                    <div style={{
-                                        backgroundColor: color, width: "10px",
-                                        height: "10px",
-                                        border: "none",
-                                        borderRadius: "50%",
-                                    }} key={index}></div>
-                                ))}
-                            </div>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-            </Col>
-        );
-    };
+    }, [dispatch]);
 
     return (
-        <Container>
-            <h1 style={{ textAlign: 'center', marginBottom: '20px', fontFamily: 'Arial, sans-serif', fontSize: '24px' }}>Adımlar</h1>
-            <Row>
-                {/* Adımlar Sağ Tarafta */}
-                <Col md={4} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-                    <div style={{ padding: '5px', background: '#f1f1f1', borderRadius: '10px', textAlign: 'center', fontSize: '14px' }}>
-                        <h6>1. Fotoğrafını Yükle</h6>
-                    </div>
-                    <div style={{ padding: '5px', background: '#f1f1f1', borderRadius: '10px', textAlign: 'center', fontSize: '14px' }}>
-                        <h6>2. Yapay Zeka Önerisini Gör</h6>
-                    </div>
-                    <div style={{ padding: '5px', background: '#f1f1f1', borderRadius: '10px', textAlign: 'center', fontSize: '14px' }}>
-                        <h6>3. Ürünleri Seç</h6>
-                    </div>
-                </Col>
-
-                {/* Fotoğraf Yükleme Alanı */}
-                <Col md={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <div
-                        onClick={() => document.getElementById('fileInput').click()}
-                        style={{
-                            width: '300px',
-                            height: '200px',
-                            border: '2px dashed #ccc',
-                            borderRadius: '10px',
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '50px', flexDirection: "column" }}>
+            <div style={{ width: '60%' }}>
+                <form onSubmit={handleSubmit}>
+                    <div style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        gap: '20px',
+                        marginTop: '20px',
+                        fontFamily: 'Arial, sans-serif',
+                    }}>
+                        {/* Dosya Seç Butonu */}
+                        <label htmlFor="fileInput" style={{
                             cursor: 'pointer',
-                            backgroundColor: '#f9f9f9',
-                        }}
-                    >
-                        {selectedImage ? (
-                            <img
-                                src={URL.createObjectURL(selectedImage)}
-                                alt="Yüklenen Fotoğraf"
-                                style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '10px' }}
-                            />
-                        ) : (
-                            <p style={{ fontWeight: 'bold', color: '#888', fontSize: '12px' }}>Fotoğraf yüklemek için tıklayın</p>
-                        )}
+                            padding: '12px 30px',
+                            backgroundColor: '#376642',
+                            color: 'white',
+                            borderRadius: '8px',
+                            fontWeight: 'bold',
+                            textAlign: 'center',
+                            fontSize: '16px',
+                            transition: 'background-color 0.3s ease',
+                        }}>
+                            Resim Seç
+                        </label>
                         <input
+                            style={{
+                                display: 'none',
+                            }}
                             type="file"
                             id="fileInput"
-                            style={{ display: 'none' }}
                             accept="image/*"
                             onChange={handleImageChange}
                         />
-                    </div>
-                </Col>
-            </Row>
 
-            {/* Ürünleri Listele */}
-            <Row>
-                {loading && <p>Ürünler yükleniyor...</p>}
-                {error && <p style={{ color: 'red' }}>{error}</p>}
-                {personalProducts && personalProducts.length > 0 ? (
-                    personalProducts.map(product => <Product key={product.id} product={product} />)
-                ) : (
-                    <p>Henüz ürün yok.</p>
-                )}
-            </Row>
-        </Container>
+                        {/* Resim Önizleme */}
+                        {selectedImage && (
+                            <div style={{
+                                width: '40%',
+                                height: '60%',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                marginTop: '20px',
+                                borderRadius: '8px',
+                                overflow: 'hidden',
+                                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+                            }}>
+                                <img
+                                    src={URL.createObjectURL(selectedImage)}
+                                    alt="Yüklenen Fotoğraf"
+                                    style={{
+                                        width: '100%',
+                                        height: 'auto',
+                                        objectFit: 'cover',
+                                    }}
+                                />
+                            </div>
+                        )}
+
+                        {/* Yükle Butonu */}
+                        <button style={{
+                            padding: '12px 30px',
+                            backgroundColor: '#000',
+                            color: 'white',
+                            fontWeight: 'bold',
+                            border: 'none',
+                            borderRadius: '8px',
+                            cursor: 'pointer',
+                            fontSize: '16px',
+                            transition: 'background-color 0.3s ease',
+                            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+                        }}
+                                type="submit">
+                            Kombin Öner
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+            {/* Ürün Kartlarını Listele */}
+            {loading ? (
+                <p>Yükleniyor...</p>
+            ) : error ? (
+                <p style={{ color: 'red' }}>{error}</p>
+            ) : (
+                <div style={{ marginTop: '30px', display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+                    {personalProducts.map((product) => (
+                        <Product key={product.id} product={product} />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+}
+
+function Product({ product }) {
+    if (!product) {
+        return <div>Ürün bulunamadı</div>;
+    }
+
+    const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "black"];
+    const { id, name, size, material, brand, price, imageName } = product;
+    const navigate = useNavigate();
+    const [liked, setLiked] = useLikeState(false); // Favori durumu için state
+
+    const handleLike = () => {
+        setLiked(!liked); // Favori durumunu değiştir
+    };
+
+    return (
+        <Card
+            onClick={() => navigate(`/products/${id}`)}
+            sx={{ width: 370, height: 600 }}
+        >
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="500"
+                    image={imageName ? `http://localhost:8080/api/products/file/${imageName}` : "http://localhost:8080/api/products/file/product_2_05435615-152f-45d0-b756-4219f563f306.jpg"}
+                    alt={name}
+                />
+                <CardContent style={{
+                    textAlign: "center",
+                    fontSize: "5px",
+                }}>
+                    <Typography style={{
+                        textTransform: "uppercase",
+                        fontFamily: "'Platypi', 'serif'"
+                    }} gutterBottom variant="caption" component="div">
+                        {name}
+                    </Typography>
+
+                    <Typography style={{
+                        textTransform: "uppercase",
+                        fontFamily: "'Platypi', 'serif'",
+                        fontWeight: "bolder"
+                    }} variant="body2">
+                        {price},00 TL
+                    </Typography>
+
+                    {/* Favori ikonu */}
+                    <div
+                        onClick={(e) => {
+                            e.stopPropagation(); // Kartın tıklanmasını engellemek için
+                            handleLike();
+                        }}
+                        style={{
+                            position: "absolute",
+                            bottom: "20px",
+                            right: "20px",
+                            backgroundColor: "white",
+                            borderRadius: "50%",
+                            padding: "10px",
+                            cursor: "pointer",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            boxShadow: liked ? "0 4px 8px rgba(0, 0, 0, 0.3)" : "0 4px 8px rgba(0, 0, 0, 0.1)",
+                        }}
+                    >
+                        <FavoriteIcon style={{ color: liked ? "red" : "black" }} />
+                    </div>
+
+                    <div style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        gap: "4px",
+                        marginTop: "10px",
+                    }}>
+                        {colors.map((color, index) => (
+                            <div style={{
+                                backgroundColor: color, width: "10px",
+                                height: "10px",
+                                border: "none",
+                                borderRadius: "50%",
+                            }} key={index}></div>
+                        ))}
+                    </div>
+                </CardContent>
+            </CardActionArea>
+        </Card>
     );
 }
 
